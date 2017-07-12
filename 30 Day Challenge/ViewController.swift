edc7f4d91d14e25a4eb42487f1cbe414cb9ed9ad
Anshula Singh <anshula.singh@gmail.com>
2017-07-12 09:37:37 -0700

    Initial Commit

---
+++ 30 Day Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  30 Day Challenge
+//
+//  Created by Anshula Singh on 7/12/17.
+//  Copyright © 2017 Anshula Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
